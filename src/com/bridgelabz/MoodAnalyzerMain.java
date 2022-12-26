d2a747c716b774d783e18e3cb0db1e045f074b5c
Prince <princek2530@gmail.com>
2022-12-26 11:36:16 +0530

    Added: Given I am in Any Mood message Should Return HAPPY

--- src/com/bridgelabz/MoodAnalyzerMain.java
+++ src/com/bridgelabz/MoodAnalyzerMain.java
@@ -4,6 +4,6 @@ public class MoodAnalyzerMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Mood Analyzer Program");
         MoodAnalyzer mood = new MoodAnalyzer();
-        System.out.println(mood.analyzeMood("i am in sad mood"));
+        System.out.println(mood.analyzeMood("i am in happy mood"));
     }
 }
